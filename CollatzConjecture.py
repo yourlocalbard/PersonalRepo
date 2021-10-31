1d9494739a126d81982d22c819f9f4e18b96780a
Jacob Zaionz <73929108+yourlocalbard@users.noreply.github.com>
2021-10-31 01:37:00 -0700

    Updated CollatzConjecture.py
    This file works for the moment. I want to do more with it though.

--- CollatzConjecture.py
+++ CollatzConjecture.py
@@ -4,3 +4,29 @@ import math
 #If even: x/2
 #If odd: 3x + 1
 
+def selectInput():
+    integer = int(input("Please select a positive integer: "))
+    print(integer)
+    while(integer > 1):
+        if(integer % 2 != 0):
+            integer = 3 * integer + 1
+            print(integer)
+        if(integer % 2 == 0):
+            integer = integer / 2
+            print(integer)
+    if(integer == 1):
+        integer = 3 * integer + 1
+        print(integer)
+        integer = integer / 2
+        print(integer)
+        integer = integer / 2
+        print(integer)
+
+    print("Thank you for using this program!")
+
+def main():
+    print("Welcome to the Collatz Conjecture Program")
+    selectInput()
+
+
+main()
