95c4aa4f3cb39da6a4471ad2ec6d45811e895e44
jzaionz@mail.sfsu.edu <jzaionz@mail.sfsu.edu>
2022-06-06 16:01:08 -0700

    Fixed a double

--- Python Projects/CollatzConjecture.py
+++ Python Projects/CollatzConjecture.py
@@ -1,32 +1,37 @@
-import math
-
 #So the theorem is as follows:
 #If even: x/2
 #If odd: 3x + 1
-
-def selectInput():
-    integer = int(input("Please select a positive integer: "))
-    print(integer)
-    while(integer > 1):
-        if(integer % 2 != 0):
-            integer = 3 * integer + 1
-            print(integer)
-        if(integer % 2 == 0):
-            integer = integer / 2
-            print(integer)
-    if(integer == 1):
+print("=====================================================================")
+print("Welcome to the Collatz Conjecture Program")
+roundCount = int(1)
+integer = int(input("Please select a positive integer: "))
+print(integer)
+while(integer > 1):
+    if(integer % 2 != 0):
         integer = 3 * integer + 1
-        print(integer)
+        #We use the .format to make sure we can actually print the integers
+        printableCounter = "{roundcount}"
+        printableInteger = "{value}"
+        print(printableCounter.format(roundcount=roundCount) + ") " + printableInteger.format(value=integer))
+        roundCount = roundCount + 1
+    if(integer % 2 == 0):
         integer = integer / 2
-        print(integer)
-        integer = integer / 2
-        print(integer)
+        printableCounter = "{roundcount}"
+        printableInteger = "{value}"
+        print(printableCounter.format(roundcount=roundCount) + ") " + printableInteger.format(value=integer))
+        roundCount = roundCount + 1
+#This was used to be a confirmation that the conjecture ends
+#if(integer == 1):
+#    integer = 3 * integer + 1
+#    print(integer)
+#    integer = integer / 2
+#    print(integer)
+#    integer = integer / 2
+#    print(integer)
 
-    print("Thank you for using this program!")
-
-def main():
-    print("Welcome to the Collatz Conjecture Program")
-    selectInput()
-
-
-main()
+printInt = str(roundCount - 1)
+print("=====================================================================")
+print("This theorem runs " + printInt + " times before terminating")
+print(" ")
+print("Thank you for using this program!")
+print("=====================================================================")
