a982c4ae7509be07d44ec26c1f12a5aca3e30a37
jzaionz@mail.sfsu.edu <jzaionz@mail.sfsu.edu>
2022-12-10 13:44:39 -0800

    Began the foundations of the ETH prediction project, but trying to understand why imports aren't working.

---
+++ Math_400_Project/main.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as wb
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import norm
+import datetime
+
+file = pd.read_csv("ETH-USD.csv", parse_dates=True)
+date = pd.to_datetime(file.time)
+data = file.close
+#plt.plot(date, data)
+#plt.show()
+
+log_returns = np.log(1 + data.pct_change())
+u = log_returns.mean()
+var = log_returns.var()
+drift = u - (0.5*var)
+
+stdev = log_returns.std()
+days = 30 # 1 month prediction
+iterations = 100
+Z = norm.ppf(np.random.rand(days, iterations))
+daily_returns = np.exp(drift + stdev * Z)
+
+price_paths = np.zeros_like(daily_returns)
+price_paths[0] = data.iloc[0]
+for t in range(1, days):
+    price_paths[t] = price_paths[t-1]*daily_returns[t]
+
+base = date.iloc[0]
+future_dates = [base + datetime.timedelta(days=x) for x in range(days)]
+
+plt.plot(date, data, linestyle='dashed')
+plt.plot(future_dates, price_paths)
+plt.ylabel("Close price (ETH/USD)")
+plt.show()
+
+sns.histplot(price_paths[-1,:], bins = 20, stat = "frequency")
+plt.xlabel("Close price (ETH/USD)")
+plt.ylabel("Frequency")
+plt.show()
