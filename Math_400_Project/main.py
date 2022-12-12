8ba653a719edc8d6c33323cda302551bfbee3f5d
jzaionz@mail.sfsu.edu <jzaionz@mail.sfsu.edu>
2022-12-11 21:40:54 -0800

    Added comments to explain code and math logic

--- Math_400_Project/main.py
+++ Math_400_Project/main.py
@@ -7,41 +7,47 @@ from scipy.stats import norm
 import datetime
 
 #Import and read the CSV file
-file = pd.read_csv("Math_400_Project\ETH-USD.csv", parse_dates=True)
-date = pd.to_datetime(file.Date)
-data = file.Close
+csv = pd.read_csv("Math_400_Project\ETH-USD.csv", parse_dates=True)
+date = pd.to_datetime(csv.Date)
+closingValue = csv.Close
 
 #Plotting to show the history of ETH-USD valuation
-plt.plot(date, data)
+plt.title("ETH-USD Historical Data")
+plt.plot(date, closingValue)
+plt.xlabel("Date")
+plt.ylabel("USD Value/ETH")
 plt.show()
 
-log_returns = np.log(1 + data.pct_change())
-u = log_returns.mean()
-var = log_returns.var()
-drift = u - (0.5*var)
+#Calculating the Monte Carlo Simulations
+#Log Returns = (1 + log(closingValue(%)))
+logReturns = np.log(1 + closingValue.pct_change()) 
+u = logReturns.mean() #Mean Value
+var = logReturns.var() #Var Value
+D = u - (0.5*var) #Drift = mean - 1/2(deviation^2)
 
-stdev = log_returns.std()
-days = 30 # 1 month prediction
-iterations = 100
-Z = norm.ppf(np.random.rand(days, iterations))
-daily_returns = np.exp(drift + stdev * Z)
+#Calculate a 1 year projection
+stdev = logReturns.std() #Calculate the standard deviation
+days = 365 #1 year prediction
+iterations = 100 #More iterations can provide more accurate results
+Z = norm.ppf(np.random.rand(days, iterations)) 
+dailyReturns = np.exp(D + stdev * Z)
 
-price_paths = np.zeros_like(daily_returns)
-price_paths[0] = data.iloc[0]
+#Calculating the individual price paths for projections
+projectedValue = np.zeros_like(dailyReturns) 
+projectedValue[0] = closingValue.iloc[0]
 for t in range(1, days):
-    price_paths[t] = price_paths[t-1]*daily_returns[t]
-
+    projectedValue[t] = projectedValue[t-1]*dailyReturns[t]
 base = date.iloc[0]
-future_dates = [base + datetime.timedelta(days=x) for x in range(days)]
+futureDates = [base + datetime.timedelta(days=x) for x in range(days)]
 
 #Printing line graph with potential projected values
-plt.plot(date, data, linestyle='dashed')
-plt.plot(future_dates, price_paths)
+plt.plot(date, closingValue, linestyle='dashed')
+plt.plot(futureDates, projectedValue)
 plt.ylabel("Close price (ETH/USD)")
 plt.show()
 
 #Printing plot for valuation frequency
-sns.histplot(price_paths[-1,:], bins = 20, stat = "frequency")
+sns.histplot(projectedValue[-1,:], bins = 20, stat = "frequency")
 plt.xlabel("Close price (ETH/USD)")
 plt.ylabel("Frequency")
 plt.show()
